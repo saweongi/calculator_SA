8275c85936970dcb47e0cc18de7a4a9bdd070a82
weongi <wwdnjsrl2@naver.com>
2023-06-20 11:34:14 +0900

    로또 기능 및 clear 버튼 추가

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import sys 
 from PyQt5.QtWidgets import (QApplication, 
-                             QWidget,QPushButton,QVBoxLayout,QMessageBox, QPlainTextEdit)
+                             QWidget,QPushButton,QVBoxLayout,
+                             QMessageBox, QPlainTextEdit, QHBoxLayout)
 from PyQt5.QtGui import QIcon
+import random 
+
+
+
 class Calculator(QWidget):
     
     def __init__(self):
@@ -14,10 +19,18 @@ class Calculator(QWidget):
 
         self.btn1 = QPushButton('Message', self)
         self.btn1.clicked.connect(self.activateMessage)
+        self.btn2 = QPushButton('Clear', self)
+        self.btn2.clicked.connect(self.clearMessage)
+
+        hbox = QHBoxLayout()
+        hbox.addStretch(1)
+        hbox.addWidget(self.btn1)
+        hbox.addWidget(self.btn2)
+
 
         vbox = QVBoxLayout()
         vbox.addWidget(self.te1)
-        vbox.addWidget(self.btn1)
+        vbox.addLayout(hbox)
         vbox.addStretch(1)
 
 
@@ -29,7 +42,11 @@ class Calculator(QWidget):
         self.show()
 
     def activateMessage(self):
-        self.te1.appendPlainText("Button clicked!")
+        lottoNum = random.sample(range(1,46),6)
+        self.te1.appendPlainText(str(lottoNum))
+
+    def clearMessage(self):
+        self.te1.clear()
 
 if __name__ =='__main__': # 이 파일을 직접 실행할 시에만 명령을 수행하겠다!.
     app = QApplication(sys.argv)
